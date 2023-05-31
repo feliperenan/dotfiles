7afe8997fba82d6ce534ae9b3c6baef287a1218a
Felipe Renan <feelipe.renan@gmail.com>
2023-05-31 16:11:19 +0100

    Update lvim config

--- lvim/config.lua
+++ lvim/config.lua
@@ -10,7 +10,8 @@ ln -s ~/dotfiles/lvim/config.lua ~/.config/lvim/config.lua
 -- General
 lvim.log.level = "warn"
 lvim.format_on_save = true
-lvim.colorscheme = "tokyonight-storm"
+-- lvim.colorscheme = "tokyonight-storm"
+lvim.colorscheme = "catppuccin"
 vim.o.background = "dark"
 vim.o.relativenumber = true
 vim.o.ls = 0
@@ -25,7 +26,7 @@ vim.cmd [[
 ]]
 
 -- VIM TEST
-vim.cmd [[ let g:test#strategy = "vimux" ]]
+vim.cmd [[ let g:test#strategy = "toggleterm" ]]
 
 -- don't close the terminal by default.
 -- vim.cmd [[ let g:test#neovim#start_normal = 1 ]]
@@ -34,7 +35,6 @@ vim.cmd [[ let g:test#strategy = "vimux" ]]
 vim.cmd [[ let g:startify_relative_path = 1 ]]
 vim.cmd [[ let g:startify_change_to_vcs_root = 1 ]]
 vim.cmd [[ set termguicolors ]]
-
 
 -- CUSTOM MAPPINGS
 -- keymappings [view all the defaults by pressing <leader>Lk]
@@ -74,7 +74,7 @@ lvim.builtin.nvimtree.setup.actions.change_dir.restrict_above_cwd = true
 lvim.builtin.project.manual_mode = true -- keep file tree in the opened directory.
 lvim.builtin.bufferline.mode = "buffers"
 lvim.builtin.lualine.active = false
-
+lvim.lsp.buffer_mappings.normal_mode['<Tab>'] = nil
 
 lvim.builtin.treesitter.ensure_installed = {
   "bash",
@@ -83,25 +83,28 @@ lvim.builtin.treesitter.ensure_installed = {
   "lua",
   "css",
   "yaml",
-  "elixir"
+  -- "elixir"
 }
 
 lvim.builtin.treesitter.ignore_install = { "haskell" }
 lvim.builtin.treesitter.highlight.enabled = true
 lvim.builtin.breadcrumbs.active = false
-lvim.lsp.diagnostics.virtual_text = false
+
+-- lvim.lsp.diagnostics.virtual_text = false
+vim.diagnostic.config({ virtual_text = false })
 
 -- Additional Plugins
 lvim.plugins = {
   -- Colorschemes
   { 'folke/tokyonight.nvim' },
-  -- { 'sainnhe/everforest' },
+  { 'sainnhe/everforest' },
+  { 'catppuccin/nvim' },
 
   -- VIM test
   -- Vimux to run commands from vim on tmux. I use it mostly for running tests
   -- over the cursor.
   { "vim-test/vim-test" },
-  { 'preservim/vimux' },
+  -- { 'preservim/vimux' },
 
   -- Add :Rename command so I can rename files as :Rename old new
   { 'danro/rename.vim' },
@@ -114,11 +117,11 @@ lvim.plugins = {
   -- VIM surround
   { "tpope/vim-surround" },
 
-  -- Display indentation.
-  { 'Yggdroot/indentLine' },
-
   -- Multi line cursor.
-  { 'mg979/vim-visual-multi' }
+  { 'mg979/vim-visual-multi' },
+
+  -- GITHUB copilot
+  -- { 'github/copilot.vim' },
 }
 
 vim.filetype.add({
